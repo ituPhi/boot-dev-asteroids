b380ea50a3dbd2b7a9bd548d6c5f9e79406822e2
i2phi <jfiturbe@gmail.com>
2025-11-12 05:47:35 -0500

    update: refactor game loop

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-def main():
-    print("Hello from boot-dev-asteroids!")
+import pygame
 
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+
+
+def main():
+    pygame.init()
+    print(f"Starting Asteroids with pygame version:{pygame.version.ver}")
+    print(f"""
+Screen width: {SCREEN_WIDTH}
+Screen height: {SCREEN_HEIGHT}
+        """)
+
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+running = True
+
+while running:
+    log_state()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print("Exiting...")
+            running = False
+
+    screen.fill("black")
+    pygame.display.flip()
 
 if __name__ == "__main__":
     main()
